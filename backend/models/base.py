3ffaf229961c1895468499a8ec89c9dcba6828ed
Tihon <miha311008@gmail.com>
2026-04-05 10:59:08 +0300

    feat(models): Добавлены типы и маппинг для моделей SQLAlchemy
    - intpk: первичный ключ int с autoincrement
    - created_at: timestamp создания записи (UTC)
    - updated_at: timestamp обновления записи (UTC)
    - str_200, str_32, str_64: строки фиксированной длины
    
    Настроен type_annotation_map в Base для автоматического
    преобразования типов в String

---
+++ backend/models/base.py
@@ -0,0 +1,30 @@
+from sqlalchemy.orm import mapped_column, DeclarativeBase
+from sqlalchemy import text, String
+from datetime import datetime
+from typing import Annotated
+
+intpk = Annotated[int, mapped_column(primary_key=True, autoincrement=True)]
+created_at = Annotated[datetime, mapped_column(
+    server_default=text("TIMEZONE('utc', NOW())"),
+    nullable=False
+)]
+
+updated_at = Annotated[
+        datetime,
+        mapped_column(
+            server_default=text("TIMEZONE('utc', now())"),
+            onupdate=datetime.utcnow
+        )
+]
+
+str_200 = Annotated[str, 200]
+str_32 = Annotated[str, 32]
+str_64 = Annotated[str, 64]
+
+class Base(DeclarativeBase):
+    type_annotation_map = {
+        str_200: String(200),
+        str_32: String(32),
+        str_64: String(64)
+    }
+    pass
