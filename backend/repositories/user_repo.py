62dd04f3b832b72f5c3cc02bfc92df0a527c70f2
Tihon <miha311008@gmail.com>
2026-04-13 19:41:58 +0300

    feat(repositories): Закончил user_repo и добавил update и delete
    - универсальный метод update_user, принимает атрибут (user_id, login,
      email, hash_pass) и меняет на newdata (новую информацию), возвращает
      булевое значение
    - метод delete_user, принимает user_id (UUID) и удаляет его из базы
      данных, возвращает значение булево значение

--- backend/repositories/user_repo.py
+++ backend/repositories/user_repo.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Session
-from sqlalchemy import UUID, select
+from sqlalchemy import UUID, select, update, delete
 from models.user import User
 
 class UserRepository:
@@ -10,10 +10,29 @@ class UserRepository:
         user = User(login=f_login, hash_pass=f_hash_pass, email=f_email)
         self.db.add(user)
         await self.db.commit()
-        await self.db.close()
         return user
     
     async def get_user(self, attribute: str, argument: UUID | str) -> User | None:
         stmt = select(User).where(getattr(User, attribute) == argument)
         user = await self.db.execute(stmt)
         return user.scalar_one_or_none()
+    
+    async def update_user(self, attribute: str, olddata: UUID | str, newdata: str) -> bool:
+        stmt = (
+            update(User)
+            .where(getattr(User, attribute) == olddata)
+            .values({attribute: newdata})
+        )
+        result = await self.db.execute(stmt)
+        await self.db.commit()
+        return True if result.rowcount == 1 else False
+
+        
+    async def delete_user(self, pk_uuid: UUID) -> bool:
+        stmt = (
+            delete(User)
+            .where(User.user_id == pk_uuid)
+        )
+        result = await self.db.execute(stmt)
+        await self.db.commit()
+        return True if result.rowcount == 1 else False
