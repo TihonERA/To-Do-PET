e1778896d909ba18b407c87bfb684075f7a7fc65
Tihon <miha311008@gmail.com>
2026-04-21 21:23:23 +0300

    feat(utils): добавил ошибку родитель DomainError и её наследника InvalidCredentialsError

--- backend/utils/validators.py
+++ backend/utils/validators.py
@@ -1,3 +1,9 @@
+class DomainError(Exception):
+    def __init__(self, detail: str, status_code: int = 400):
+        self.detail = detail
+        self.status_code = status_code
+        super().__init__(detail)
+
 class AlreadyTaken(Exception):
     def __init__(self, detail: str):
         self.detail = detail
@@ -12,6 +18,10 @@ class ValidationError(Exception):
     def __init__(self, detail: str):
         self.detail = detail
         super().__init__(detail)
+
+class InvalidCredentialsError(DomainError):
+    def __init__(self):
+        super().__init__(detail="Invalid credentials", status_code=401)
 
 def validate_pass(password: str):
     if len(password) < 16:
