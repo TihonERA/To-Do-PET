7adef1984d16fb1144df2c91e717e3ba08a2eee2
Tihon <miha311008@gmail.com>
2026-04-30 20:32:18 +0300

    refactor(api/v1): добавил пайдантик схемы в auth

--- backend/api/v1/auth.py
+++ backend/api/v1/auth.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException, Form
 from typing import Annotated
 from backend.services.auth_serivce import AuthService
 from backend.utils.validators import AlreadyTaken, InvalidCredentialsError
-from backend.schemas.user import Token
+from backend.schemas.user import Token, UserLogin, UserRegister
 from backend.api.deps import get_user_auth_service
 
 
@@ -10,25 +10,22 @@ router = APIRouter(tags=["Authentication"])
 
 @router.post("/register", response_model=Token)
 async def register(
-    email: Annotated[str, Form()], 
-    login: Annotated[str, Form()], 
-    password: Annotated[str, Form()], 
+    data: UserRegister, 
     user_auth: Annotated[AuthService, Depends(get_user_auth_service)]
     ) -> Token:
     try:
-        token = await user_auth.register_user(login, email, password)
+        token = await user_auth.register_user(data.login, data.email, data.password)
     except AlreadyTaken as e:
         raise HTTPException(status_code=409, detail=e.detail)
     return token
 
 @router.post("/login", response_model=Token)
 async def login(
-    login_or_email: Annotated[str, Form()], 
-    password: Annotated[str, Form()],
+    data: UserLogin,
     user_auth: Annotated[AuthService, Depends(get_user_auth_service)]
     ) -> Token:
     try:
-        token = await user_auth.login_user(login_or_email, password)
+        token = await user_auth.login_user(data.login_or_email, data.password)
         return token
     except InvalidCredentialsError as e:
         raise HTTPException(status_code=e.status_code, detail=e.detail)
