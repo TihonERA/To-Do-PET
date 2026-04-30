e3f58b2d4bd0e169c3857ffc49f50487d3c96dfc
Tihon <miha311008@gmail.com>
2026-04-30 20:26:56 +0300

    refactor(api/v1): добавил пайдантик схемы в users

--- backend/api/v1/users.py
+++ backend/api/v1/users.py
@@ -3,7 +3,7 @@ from typing import Annotated
 from backend.models.user import User
 from backend.services.user_service import UserService
 from backend.utils.validators import NotFound, AlreadyTaken, ValidationError, InternalServerError
-from backend.schemas.user import UserResponse
+from backend.schemas.user import UserResponse, UserUpdateProfile, UserUpdatePassword, UserDeleteAccount
 from backend.api.deps import get_current_user, get_user_service
 
 router = APIRouter(tags=["User"])
@@ -16,16 +16,15 @@ async def get_current_user(
 
 @router.patch("/users/update_profile", response_model=UserResponse)
 async def update_profile(
-    new_login: Annotated[str | None, Form()],
-    new_email: Annotated[str | None, Form()],
+    data: UserUpdateProfile,
     current_user: Annotated[User, Depends(get_current_user)],
     user_service: Annotated[UserService, Depends(get_user_service)]
     ) -> UserResponse:
     try:
         return await user_service.update_profile(
             user_id=current_user.user_id,
-            new_login=new_login,
-            new_email=new_email
+            new_login=data.new_login,
+            new_email=data.new_email
         )
     except NotFound as e:
         raise HTTPException(status_code=404, detail=e.detail)
@@ -34,16 +33,15 @@ async def update_profile(
     
 @router.patch("/users/update_password")
 async def update_password(
-    old_password: Annotated[str, Form()],
-    new_password: Annotated[str, Form()],
+    data: UserUpdatePassword,
     current_user: Annotated[User, Depends(get_current_user)],
     user_service: Annotated[UserService, Depends(get_user_service)]
     ) -> dict:
     try:
         await user_service.change_password(
             user_id=current_user.user_id,
-            old_password=old_password,
-            password=new_password
+            old_password=data.old_password,
+            password=data.new_password
         )
         return {"detail": "Password successfully changed"}
     except NotFound as e:
@@ -53,14 +51,14 @@ async def update_password(
     
 @router.delete("/users/delete_account")
 async def delete_account(
+    data: UserDeleteAccount,
     current_user: Annotated[User, Depends(get_current_user)],
-    password: Annotated[str, Form()],
     user_service: Annotated[UserService, Depends(get_user_service)]
     ):
     try:
         user_service.delete_account(
             user_id=current_user.user_id,
-            password=password
+            password=data.password
         )
         return {"detail": "Account deleted"}
     except NotFound as e:
