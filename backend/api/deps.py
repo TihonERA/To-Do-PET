27e2d826a44dc4d9efb27e3a421b66fcf2f0152e
Tihon <miha311008@gmail.com>
2026-04-30 21:00:57 +0300

    feat(api): добавил зависимость для пайдантик схемы

--- backend/api/deps.py
+++ backend/api/deps.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, HTTPException
+from fastapi import Depends, HTTPException, Query
 from fastapi.security import OAuth2PasswordBearer
 from typing import Annotated
 from backend.services.user_service import UserService
@@ -9,10 +9,29 @@ from backend.repositories.user_repo import UserRepository
 from backend.core.config import settings
 from backend.core.database import get_db
 from backend.utils.validators import NotFound
+from backend.schemas.task import TaskFilterParams
 from sqlalchemy.ext.asyncio import AsyncSession
+from typing import Optional
 import jwt
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+def get_task_filters(
+    skip: int = Query(0, ge=0),
+    limit: int = Query(100, ge=1, le=500),
+    sort_by: str = Query("created"),
+    sort_desc: bool = Query(True),
+    status: Optional[bool] = Query(None),
+    priority: Optional[int] = Query(None, ge=0, le=3)
+) -> TaskFilterParams:
+    return TaskFilterParams(
+        skip=skip,
+        limit=limit,
+        sort_by=sort_by,
+        sort_desc=sort_desc,
+        status=status,
+        priority=priority
+    )
 
 def get_user_repo(db: AsyncSession = Depends(get_db)):
     return UserRepository(db=db)
