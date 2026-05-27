ac3ba3ed58f2ed5aec25dbf7aa7f7518df7097b7
YouCutDream <sorsvlad@gmail.com>
2026-05-27 22:11:54 +0700

    feat(Frontend)изменил код в login.js

--- Frontend/js/login.js
+++ Frontend/js/login.js
@@ -1,47 +1,36 @@
-const form = document.querySelector('.form-box form');
+    const form = document.querySelector('.form-box form');
     const loginInput = document.querySelector('.login');
     const passwordInput = document.querySelector('.pass');
-    const submitBtn = document.querySelector('button[type="submit"]');
-    let errorDiv = document.querySelector('.login-error');
-    if (!errorDiv) {
-        errorDiv = document.createElement('div');
-        errorDiv.className = 'login-error';
-        errorDiv.style.color = 'red';
-        errorDiv.style.marginTop = '10px';
-        errorDiv.style.textAlign = 'center';
-        form.appendChild(errorDiv);
-    }
+    const submitBtn = document.querySelector('button');
+    const token = localStorage.getItem('token')
+
+    const responce = await fetch('http://localhost:8000/login' , {
+        headers: {'Authorization': `Bearer ${token}`}
+    })
+
     form.addEventListener('submit', async (e) => {
         e.preventDefault(); 
-        errorDiv.textContent = '';
         const login = loginInput.value.trim();
         const password = passwordInput.value;
-
         if (!login || !password) {
-            errorDiv.textContent = 'Заполните все поля';
-            return;
-        }
-        try {
-            const response = await fetch('http://localhost:8000/registration', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ login, password })
-            });
+            alert('Заполните все поля');
+            return 
+        }})
 
-            const data = await response.json();
+    submitBtn.addEventListener('click' , () => {
+        e.preventDefault() 
+        window.location.href = 'http://localhost:8000/'
+    })
 
-            if (response.ok) {
-                window.location.href = '/http://localhost:8000/registration';
-            } else {
-                // Ошибка авторизации – показываем сообщение от сервера
-                errorDiv.textContent = data.message || 'Неверный логин или пароль';
-            }
-        } catch (err) {
-            console.error('Ошибка соединения:', err);
-            errorDiv.textContent = 'Сервер временно недоступен. Попробуйте позже.';
-        } finally {
-            // Разблокируем кнопку
-            submitBtn.disabled = false;
-            submitBtn.textContent = 'Вход';
-        }
-    });
+    if(!login.length < 0){
+        alert('Введите логин')
+        return
+    } 
+    if(!password.length < 0){
+        alert('Введите пароль')
+        return
+    }
+    if(token){
+        window.location.href = 'http://localhost:8000/'
+    }
+            
